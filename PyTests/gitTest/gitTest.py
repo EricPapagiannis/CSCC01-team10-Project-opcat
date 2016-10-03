07dc1d3145ab42c454d591863845bb48152966d8
Conanap <albionapc@gmail.com>
2016-10-03 15:33:35 -0400

    Updated the code to fix a problem where it would not set upstream properly due to missing part of git command "push". Don't ask how I missed that.

--- PyTests/gitTest/gitTest.py
+++ PyTests/gitTest/gitTest.py
@@ -4,25 +4,27 @@ from subprocess import call
 
 
 files = [];
+direc = "gittut";
 
 def initGit():
     link = input("Please type github branch link:\n");
     
     call(["git", "--bare", "clone", link]);
-    call(["git", "--set-upstream", "origin", "master"]);
-    #return link;
+    link = link.split('/')[-1][0:-4];
+    call(["git", "push", "--set-upstream", "origin", "master"], cwd=link);
+    return link;
     
 
 
 # check for local github repo
 out = Popen(["cd", "gittut"], stderr=PIPE);
 if("" != out.stderr.read()):
-    initGit();
+    direct = initGit();
 
 # updating github repo
 call(["git", "pull"], cwd='gittut');
 
 # commit process
-call(["git", "add"] + files, cwd='gittut');
+call(["git", "add", "."] + files, cwd='gittut');
 call(["git", "commit", "-m", "automated commit"], cwd='gittut');
 call(["git", "push"], cwd='gittut');
