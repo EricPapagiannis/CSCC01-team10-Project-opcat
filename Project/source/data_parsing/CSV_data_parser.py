212b62a6d41337f61c5f89198b92190e543ff4c4
Conanap <albionapc@gmail.com>
2016-10-22 21:49:18 -0400

    Fixed code to work properly in python 3 as well as the new planet classes

--- Project/source/data_parsing/CSV_data_parser.py
+++ Project/source/data_parsing/CSV_data_parser.py
@@ -1,26 +1,32 @@
-def buildPlanet(line):
-    _data_field = dict();
+from Planet import Planet
+def buildPlanet(line, heads):
+    _data_field = dict()
     _name = 0;
     _wanted = ["mass", "radius", "orbital_period"]
     for i in _wanted:
-        temp = " ".join(i.split("_"));
-        _data_field[temp] = heads.index(i);
+        temp = " ".join(i.split("_"))
+        _data_field[temp] = heads.index(i)
     
-    planetBuilder = Planet.Builder(line[_name]);
+    planet = Planet(line[_name])
     
     for i in _data_field:
-        planetBuilder.addVal(i, line[_data_field[i]]);
-        
-    planet = planetBuilder.compile();    
-    return planet;
+        planet.addVal(i, line[_data_field[i]])
 
-file = open("exoplanet.eu_catalog-2.csv", "r")
-lines = getDiff()
-heads = file.readline().split(',')
-line = file.readline()
-planets = dict()
-while(line):
-    line = line.split(',');
-    planet = buildPlanet(line)
-    palnets[planets._data["namePlanet"]] = planet
-    line = file.readline();
+    return planet
+
+def buildListPlanets():
+    file = open("exoplanet.eu_catalog-2.csv", "r")
+    heads = file.readline().split(',')
+    line = file.readline()
+    planets = dict()
+    while(line):
+        line = line.split(',')
+        planet = buildPlanet(line, heads)
+        planets[planet.data["namePlanet"]] = planet
+        line = file.readline()
+    return planets
+
+if __name__ == "__main__":
+    planets = buildListPlanets()
+    for i in planets:
+        print(str(i))
