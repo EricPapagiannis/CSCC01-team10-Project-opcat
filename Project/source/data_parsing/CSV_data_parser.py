f5b5ffef3746c8135c99ffc81e5865af4eba407a
Conanap <albionapc@gmail.com>
2016-10-23 22:53:02 -0400

    Ensure code is now working

--- Project/source/data_parsing/CSV_data_parser.py
+++ Project/source/data_parsing/CSV_data_parser.py
@@ -1,12 +1,13 @@
 from Planet import Planet
 
-eu = {"mass": "mass", "radius", "orbital_period": "period", "semi_major_axis":"semimajoraxis",
+eu = {"mass": "mass", "radius":"radius", "orbital_period": "period", "semi_major_axis":"semimajoraxis",
     "eccentricity":"eccentricity", "detection_type":"discoverymethod"}
 nasa = {}
 
-correction = {"discoverymethod":discoveryCorrection}
 discoveryCorrection = {"Radial Velocity": "RV", "Primary Transit": "transit", "Imaging":"imaging",
     "Pulsar":"timing", "Microlensing":"microlensing", "TTV":"transit", "Astrometry":"RV"}
+
+correction = {"discoverymethod":discoveryCorrection}
 
 def buildPlanet(line, heads, wanted, source):
     _data_field = dict()
@@ -15,12 +16,12 @@ def buildPlanet(line, heads, wanted, source):
 
     if(source == "eu"):
         _actual = eu
-    else (source == "nasa"):
+    else: #(source == "nasa")
         _actual = nasa
 
     for i in _wanted:
         temp = _actual[i]
-        tempval = _fixval(heads.index(i))
+        tempval = _fixVal(temp, heads.index(i))
         #if(heads.index(i) == "Other"):
         _data_field[temp] = tempval
 
@@ -45,21 +46,21 @@ def buildDictionaryPlanets(filename, wanted, source):
 
     while(line):
         line = line.split(',')
-        planet = buildPlanet(line, heads, source)
+        planet = buildPlanet(line, heads, wanted, source)
         planets[planet.data["namePlanet"]] = planet
         line = file.readline()
     return planets
 
 def buildListPlanets(filename, wanted, source):
-    tdict = buildDictionaryPlanets(filename, wanted)
+    tdict = buildDictionaryPlanets(filename, wanted, source)
     rlist = []
 
     for name in tdict:
-        rlist += tdict[name]
+        rlist += [tdict[name]]
     return rlist
 
 if __name__ == "__main__":
-    planets = buildListPlanets("exoplanet.eu_catalog-2.csv", ["mass", "radius", "orbital_period", 
-        "semi_major_axis"], "eu")
+    planets = buildListPlanets("exoplanet.eu_catalog-2.csv",
+        ["mass", "radius", "orbital_period", "semi_major_axis"], "eu")
     for i in planets:
         print(str(i))
