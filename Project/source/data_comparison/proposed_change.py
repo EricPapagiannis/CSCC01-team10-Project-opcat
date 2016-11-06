496dde1bee213a0b64c9fa9bdbbcc6c9fa247cb0
V-for-Vasili <vasili.skurydzin@mail.utoronto.ca>
2016-11-06 17:13:27 -0500

    Adding sorting algorithm for list of proposed changes (User story 5 task 1), Created a template of ProposedChangeTest;

--- Project/source/data_comparison/proposed_change.py
+++ Project/source/data_comparison/proposed_change.py
@@ -33,6 +33,19 @@ class Addition(ProposedChange):
         return s
 
 
+    def get_object_name():
+        '''
+        () -> str
+        
+        Returns the name of the PlanetaryObject to which this instance of 
+        proposed addition is referring.
+        '''
+        if self.object_ptr is not None :
+            return self.object_ptr.name
+        else:
+            return ""
+
+
 class Modification(ProposedChange):
     '''
     target_system_ptr is the pointer to PlanetaryObject instance originating 
@@ -83,6 +96,55 @@ class Modification(ProposedChange):
         s += str(self.value_in_OEC)
         s += "\n"
         return s
+    
+    def get_object_name():
+        '''
+        () -> str
+        
+        Returns the name of the PlanetaryObject to which this instance of 
+        proposed addition is referring.
+        '''        
+        if self.OEC_object is not None :
+            return self.OEC_object.name
+        else:
+            return ""    
+
+
+def merge_changes(first, second):
+    '''
+    ([ProposedChange], [ProposedChange]) -> [ProposedChange]
+    
+    Helper method for merge_sort_changes() method. Merges 2 sorted lists of
+    proposed changes; returns single sorted list.
+    '''
+    # List res will contain all the elements from both lists
+    res = []
+    # Append ProposedChange objects by lexicographical order of the name of the
+    # planetaryObject ProposedChanges are referring to
+    while len(first) != 0 and len(second) != 0 :
+        if first[0].get_object_name() > second[0].get_object_name() :
+            res.append(first.pop(0))
+        else:
+            res.append(second.pop(0))
+    # Add all the elements from the list that is not empty to the res
+    for i in [first, second] :
+        res.extend(i)
+    return res
+    
+
+def merge_sort_changes(CHANGES):
+    '''
+    ([ProposedChange]) -> [ProposedChange]
+    
+    Recursively sorts the list of proposed changes in lexicographical order by
+    the name of the object the change is referring to. Returns sorted list.
+    '''
+    mid = len(CHANGES) // 2
+    # Recursive calls on first and second halves.
+    first = merge_sort_changes(CHANGES[:mid])
+    second = merge_sort_changes(CHANGES[mid:])
+    # Merging and returning 2 sublists
+    return merge_changes(first, second)
 
 
 if __name__ == "__main__":
---
+++ Project/source/tests/ProposedChangeTest.py
@@ -0,0 +1,11 @@
+import unittest    
+from data_comparison.proposed_change import *
+
+class testing_merge(unittest.TestCase):
+
+    def test_merge1(self):
+        self.assertEqual(1, 2)
+        
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
