d608bd0e0dcde19e1631ebcc3e50d1acb79d0316
V-for-Vasili <vasili.skurydzin@mail.utoronto.ca>
2016-11-13 13:06:59 -0500

    Conflict in driver.py resolver; committiong directly

--- Project/source/driver.py
+++ Project/source/driver.py
@@ -8,13 +8,13 @@ import data_parsing.CSV_data_parser as CSV
 import data_comparison.Comparator as COMP
 import data_comparison.proposed_change as PC
 import github.gitClone as GIT
+import storage_manager.storage_manager as STORAGE
+import datetime
 
-help_string = "Opcat version 0.1\nBasic operation:\n$ driver --update   \
-Retrieves data from target catalogues (NASA, openexoplanet.eu) as a list of \
-starsystems. Retrieves data from the github database of Open Exoplanet \
-catalogue as a separate list of star systems. Compares the two lists, \
-building a list of proposed changes. (Not implemented yet) After update \
-is complete the user can view proposed changes. (Not implemented yet)\n\n"
+
+# usage string
+usage_str = "usage: driver [--help] [--update] [--output string] [--planet " \
+    + "string] [--showall | --shownumber int]\n"
 
 # link to NASA catalogue
 NASA_link = "http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nsted\
@@ -39,14 +39,13 @@ def usage():
     Example called method
     Returns NoneType
     '''
-    print("usage: driver [--help] [--update] [--output string] " +
-          "[--planet string] [--showall | --shownumber int]\n")
+    print(usage_str)
 
 
 def print_help():
     '''() -> NoneType
     '''
-    print(help_string)
+    print(STORAGE.manual())
 
 
 def clean_files():
@@ -65,19 +64,25 @@ def show_all():
     '''() -> NoneType
     Skeleton function
     '''
-    update()
+    unpack_changes()
     # sort the list of proposed changes    
     i = 0
     while i < len(CHANGES):
         show_number(i)
         i += 1
+    print("\nNumber of changes shown : " + str(len(CHANGES)))
+    print("Last update : " + str(STORAGE.config_get("last_update")))
+    # to reset last update time to default state ("Never"), and config file in
+    # general : STORAGE.clean_config_file()
+    print("End.\n")
+
 
 
 def show_range(start, end):
     '''() -> NoneType
     Skeleton function
     '''
-    update()
+    unpack_changes()
     # sort the list of proposed changes
     bothInts = isinstance(start, int) and isinstance(end, int)
     validRange = 0 <= start <= len(CHANGES) and end >= 0 and end <= len(CHANGES)
@@ -103,7 +108,7 @@ def show_number(n):
     Skeleton function
     '''
     if len(CHANGES) == 0:
-        update()
+        unpack_changes()
     if n < len(CHANGES) and n >= 0:
         print("\nShowing number : " + str(n + 1) + "\n")
         print(CHANGES[n])
@@ -120,7 +125,7 @@ def accept(n, strategy):
     Returns NoneType
     '''
     if len(CHANGES) == 0:
-        update()
+        unpack_changes()
     if n < len(CHANGES) and n >= 0:
         if (strategy == 1):
             GIT.modifyXML(CHANGES[n], n)
@@ -136,7 +141,7 @@ def accept_all():
     Skeleton function
     '''
     GIT.initGit()
-    update()
+    unpack_changes()
     i = 0
     while i < len(CHANGES):
         accept(i)
@@ -148,7 +153,7 @@ def accept_all2():
     Skeleton function
     '''
     GIT.initGit2()
-    update()
+    unpack_changes()
     i = 0
     # while i < len(CHANGES):
     while i < 25:
@@ -163,6 +168,12 @@ def deny(n):
 
 def deny_all():
     print("denied all")
+
+
+def unpack_changes():
+    # TODO : check that the last time of the update is not "Never"
+    global CHANGES
+    CHANGES = STORAGE.read_changes_from_memory()
 
 
 def update():
@@ -224,6 +235,15 @@ def update():
 
     # sort the list of proposed changes
     CHANGES = PC.merge_sort_changes(CHANGES)
+    # write the list of proposed changes to memory using storage_manager
+    STORAGE.write_changes_to_memory(CHANGES)
+    # calculate current time
+    curr_time = datetime.datetime.strftime(datetime.datetime.now(),
+                                           '%Y-%m-%d %H:%M:%S')
+    STORAGE.config_set("last_update", curr_time)
+    print("\nNumber of differences discovered : " + str(len(CHANGES)))
+    print("Current time : " + curr_time)
+    print("Update complete.\n")
 
 
 def main():
@@ -376,7 +396,6 @@ def main():
     # update
     if (update_flag):
         update()
-        print("Update complete.\n")
 
     # accept
     if (accept_flag):
