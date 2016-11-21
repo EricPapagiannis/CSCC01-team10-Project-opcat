73e965e59d2ac5b41e0539f118110b906cf74851
V-for-Vasili <vasili.skurydzin@mail.utoronto.ca>
2016-11-20 20:41:34 -0500

    Modified driver to verify that discovered changes are not blacklisted; User Story 9 - complete

--- Project/source/driver.py
+++ Project/source/driver.py
@@ -310,18 +310,27 @@ def update():
         for key in d:
             if d.get(key).__class__.__name__ != "Star":
                 d.pop(key)
-
-    # add chages from EU to the list
+    # retrieve the blacklist from memory
+    black_list = STORAGE.config_get("black_list")
+    # add chages from EU to the list (if they are not blacklisted by the user)
     for key in EU_stars.keys():
         if key in OEC_stars.keys():
-            C = COMP.Comparator(EU_stars.get(key), OEC_stars.get(key), "eu")
-            CHANGES.extend(C.proposedChangeStarCompare())
+            Comp_object = COMP.Comparator(EU_stars.get(key), 
+                                          OEC_stars.get(key), "eu")
+            LIST = Comp_object.proposedChangeStarCompare()
+            for C in LIST:
+                if (not C in black_list) and (not C in CHANGES):
+                    CHANGES.append(C)
 
     # add chages from NASA to the list
     for key in NASA_stars.keys():
         if key in OEC_stars.keys():
-            C = COMP.Comparator(NASA_stars.get(key), OEC_stars.get(key), "nasa")
-            CHANGES.extend(C.proposedChangeStarCompare())
+            Comp_object = COMP.Comparator(NASA_stars.get(key), 
+                                          OEC_stars.get(key), "nasa")
+            LIST = Comp_object.proposedChangeStarCompare()            
+            for C in LIST:
+                if (not C in black_list) and (not C in CHANGES):
+                    CHANGES.append(C)
 
     # sort the list of proposed changes
     CHANGES = PC.merge_sort_changes(CHANGES)
@@ -646,3 +655,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    
